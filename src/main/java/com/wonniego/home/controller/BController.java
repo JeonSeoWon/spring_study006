bbb0e56d9020dd668387b6f5a58d642f869fd33c
DESKTOP-SB48EH0\ici <ainshin0990@gmail.com>
2023-02-16 09:41:37 +0900

    spring_study006

---
+++ src/main/java/com/wonniego/home/controller/BController.java
@@ -0,0 +1,30 @@
+package com.wonniego.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wonniego.home.command.BCommand;
+import com.wonniego.home.command.BListCommand;
+
+@Controller
+public class BController {
+	
+	BCommand command = null;
+	
+	@RequestMapping(value = "/")
+	public String index() {
+	
+		return "index";
+	}
+	@RequestMapping(value = "/list")
+	public String list(Model model ) {
+		
+		command = new BListCommand();
+		command.excute(model);
+		
+		return "list";
+	}
+	
+
+}
